2c3d9445f59365aeb338caacf43d819e72856614
g14com0 <giacomo.furlan97@gmail.com>
2022-06-05 23:21:02 +0200

    test new var

--- gke/cluster.tf
+++ gke/cluster.tf
@@ -1,6 +1,7 @@
 resource "google_container_cluster" "primary" {
-  name               = var.name
-  location           = var.location
-  initial_node_count = var.initial_node_count
-  project            = var.project
+  name                     = var.name
+  location                 = var.location
+  initial_node_count       = var.initial_node_count
+  project                  = var.project
+  remove_default_node_pool = var.remove_default_node_pool
 }
--- gke/variables.tf
+++ gke/variables.tf
@@ -1,19 +1,24 @@
 variable "name" {
-    type = string
-    description = "(optional) describe your variable"
+  type        = string
+  description = "(optional) describe your variable"
 }
 
 variable "location" {
-    type = string
-    description = "(optional) describe your variable"
+  type        = string
+  description = "(optional) describe your variable"
 }
 
 variable "initial_node_count" {
-    type = number
-    description = "(optional) describe your variable"
+  type        = number
+  description = "(optional) describe your variable"
 }
 
 variable "project" {
-    type = string
-    description = "(optional) describe your variable"
+  type        = string
+  description = "(optional) describe your variable"
+}
+
+variable "remove_default_node_pool" {
+  type = string
+  description = "(optional) describe your variable"
 }
