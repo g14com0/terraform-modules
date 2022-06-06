d247601573f6a48d5904a6074a37545e801294ae
g14com0 <giacomo.furlan97@gmail.com>
2022-06-07 00:15:33 +0200

    new stuff

--- gke/cluster.tf
+++ gke/cluster.tf
@@ -11,4 +11,39 @@ resource "google_container_cluster" "primary" {
   release_channel {
     channel = var.release_channel
   }
+
+  node_pool {
+    name       = "default-nodepool"
+  }
+}
+
+resource "google_container_node_pool" "node_pool" {
+  for_each       = local.node_pool_name
+  name           = each.value
+  location       = local.location
+  node_locations = local.node_location
+
+  cluster    = google_container_cluster.primary.name
+  node_count = var.node_count
+  version    = var.gke_version
+
+  autoscaling {
+    min_node_count = var.min_node_count
+    max_node_count = var.max_node_count
+  }
+
+  management {
+    auto_upgrade = var.auto_upgrade
+    auto_repair  = var.auto_repair
+  }
+
+  upgrade_settings {
+    max_surge       = var.max_surge
+    max_unavailable = var.max_unavailable
+  }
+
+  node_config {
+    image_type   = var.image_type
+    machine_type = var.machine_type
+  }
 }
--- gke/variables.tf
+++ gke/variables.tf
@@ -6,7 +6,13 @@ variable "name" {
 variable "initial_node_count" {
   type        = number
   description = "The number of nodes to create in this cluster's default node pool"
-  default = 0
+  default     = 0
+}
+
+variable "node_pools_name" {
+  type = set(string)
+  description = "(optional) describe your variable"
+  default = [ "value" ]
 }
 
 variable "project" {
@@ -17,13 +23,13 @@ variable "project" {
 variable "regions" {
   type        = string
   description = "The region to host the cluster"
-  default = null
+  default     = null
 }
 
 variable "release_channel" {
-  type = string
+  type        = string
   description = "(optional) describe your variable"
-  default = "UNSPECIFIED"
+  default     = "UNSPECIFIED"
 }
 
 variable "remove_default_node_pool" {
@@ -39,5 +45,65 @@ variable "zonal" {
 variable "zone" {
   type        = list(string)
   description = "The zones to host the cluster"
-  default = []
+  default     = []
+}
+
+variable "node_count" {
+  type = number
+  description = "(optional) describe your variable"
+  default = 1
+}
+
+variable "gke_version" {
+  type = string
+  description = "(optional) describe your variable"
+  default = null
+}
+
+variable "min_node_count" {
+  type        = number
+  description = "Minimum number of nodes in the NodePool"
+  default     = 1
+}
+
+variable "max_node_count" {
+  type        = number
+  description = "Maximum number of nodes in the NodePool"
+  default     = 1
+}
+
+variable "auto_upgrade" {
+  type        = bool
+  description = "Whether the nodes will be automatically upgraded"
+  default     = false
+}
+
+variable "auto_repair" {
+  type        = string
+  description = "Whether the nodes will be automatically repaired"
+  default     = false
+}
+
+variable "max_surge" {
+  type        = number
+  description = "The number of additional nodes that can be added to the node pool during an upgrade"
+  default     = 1
+}
+
+variable "max_unavailable" {
+  type        = number
+  description = "he number of nodes that can be simultaneously unavailable during an upgrade"
+  default     = 0
+}
+
+variable "image_type" {
+  type        = string
+  description = "The default image type used by NAP once a new node pool is being created"
+  default     = "COS_CONTAINERD"
+}
+
+variable "machine_type" {
+  type        = string
+  description = "(optional) describe your variable"
+  default     = "e2-micro"
 }
