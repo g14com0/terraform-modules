96d26aec8fedebe7e44dc92f7b7a12a212306d86
giacomofurlan <56687012+g14com0@users.noreply.github.com>
2022-06-07 21:32:35 +0200

    secfix

--- gke/cluster.tf
+++ gke/cluster.tf
@@ -7,6 +7,16 @@ resource "google_container_cluster" "primary" {
   name                     = var.name
   remove_default_node_pool = var.remove_default_node_pool
 
+  ip_allocation_policy {}
+
+  private_cluster_config {
+    enable_private_nodes = true
+  }
+
+  network_policy {
+    enabled = true
+  }
+
   release_channel {
     channel = var.release_channel
   }
